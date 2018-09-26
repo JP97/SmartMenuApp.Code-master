5713ba125152282874ae52a6b2c121dfea448a61
Mia Pars Markussen <miax7855@edu.eal.dk>
2018-09-26 11:52:30 +0200

    tilføjet vores indlæs menu

--- SmartMenuLibrary/SmartMenu.cs
+++ SmartMenuLibrary/SmartMenu.cs
@@ -11,10 +11,14 @@ namespace SmartMenuLibrary
         public void LoadMenu(string path)
         {
             string Line1;
-            System.IO.StreamReader file = new System.IO.StreamReader(@"C:\Users\julia\Desktop\programmerings-projekt\SmartMenuApp.Code-master\FunctionLibrary\TextMenue.txt");
+            string Line2;
+            System.IO.StreamReader file = new System.IO.StreamReader(@"C:\Users\Mia\source\repos\SmartMenuApp.Code-master\FunctionLibrary\TextMenue.txt");
             Line1 = file.ReadLine();
+            Line2 = file.ReadLine();
+
             Console.WriteLine(Line1);
-            Console.ReadLine();
+            Console.Write(Line2);
+            Console.ReadKey();
         }
         public void Activate()
         {
