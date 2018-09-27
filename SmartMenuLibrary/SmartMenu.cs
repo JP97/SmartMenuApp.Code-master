6cc08263f09187ecbf3e0865945b2aff70139457
Mia Pars Markussen <miax7855@edu.eal.dk>
2018-09-27 12:11:56 +0200

    har lavet input fra bruger forsøgt at tjekke indtast

--- SmartMenuLibrary/SmartMenu.cs
+++ SmartMenuLibrary/SmartMenu.cs
@@ -23,17 +23,59 @@ namespace SmartMenuLibrary
                 System.Console.WriteLine(line);
                 hovedsætning += line;
                 
-
-                
             }
-
             
-
-            Console.ReadKey();
+            
+            
         }
         public void Activate()
         {
-            // Implement ...
+             
+            string line;
+            string hovedsætning = "";
+            
+            //string Line2;
+            System.IO.StreamReader file = new System.IO.StreamReader(@"C:\Users\Mia\source\repos\SmartMenuApp.Code-master\FunctionLibrary\TextMenue.txt");
+            line = file.ReadLine();
+            //Line2 = file.ReadLine();
+
+            while ((line = file.ReadLine()) != null)
+            {
+                hovedsætning += line;
+
+            }
+            string[] menuIArray= hovedsætning.Split(';');
+
+
+            Console.WriteLine("intast et tal fra menu");
+
+            int første = int.Parse(menuIArray[3]);
+            int anden = int.Parse(menuIArray[5]);
+            int trejde = int.Parse(menuIArray[7]);
+            int fjere = int.Parse(menuIArray[9]);
+
+            int menuevalg = int.Parse(Console.ReadLine());
+            
+
+            if (menuevalg >= 0 && menuevalg <= 9)
+            {
+                if (menuevalg == 3)
+                {
+                    Console.WriteLine("det er 3");
+                }
+                else if (menuevalg == 5)
+                {
+                    Console.WriteLine("punkt 5 udført");
+                }
+                else if (menuevalg == 7)
+                {
+                    Console.WriteLine("punkt 7 udført");
+                }
+                else if (menuevalg == 9)
+                {
+                    Console.WriteLine("punkt 9 udført");
+                }
+            }
         }
     }
 }
